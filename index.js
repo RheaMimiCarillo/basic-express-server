a02cd7cac417923e1b71da7ae0d951ca86e6e60c
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-09-21 23:42:35 +0000

    404 error works|logger works|index.js works|/person GET with name property works

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const server = require('./lib/server.js');
 // set the PORT to be either the one in the .env file, or 3002
 const PORT = process.env.PORT || 3002;
 
+// start the server on the specified PORT
 server.start(PORT);
